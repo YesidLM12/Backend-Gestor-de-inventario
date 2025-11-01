98db17a90d63906e4219f11a512a02d5830a218f
Yesid Londoño <yesidlondono342@gmail.com>
2025-11-01 14:32:51 -0500

    Refactor: rename base controller and update user controller; replace token schema

---
+++ App/controllers/base_controller.py
@@ -0,0 +1,35 @@
+from abc import ABC
+
+class BaseController(ABC):
+    def get_all(self, db: Session):
+        return db.query(self.model).all()
+    
+    def get_by_id(self, db: Session, id: int):
+        return db.query(self.model).filter(self.model.id == id).first()
+    
+    def create(self, db: Session, obj_in: Base):
+        db.add(obj_in)
+        db.commit()
+        db.refresh(obj_in)
+        return obj_in   
+    
+    def update(self, db: Session, id: int, obj_in: Base):
+        obj = db.query(self.model).filter(self.model.id == id).first()
+        if not obj:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Object not found')
+        obj = obj_in
+        db.add(obj)
+        db.commit()
+        db.refresh(obj)
+        return obj
+    
+    def delete(self, db: Session, id: int):
+        obj = db.query(self.model).filter(self.model.id == id).first()
+        if not obj:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Object not found')
+        db.delete(obj)
+        db.commit()
+        return obj
+    
+    def get_multi(self, db: Session, skip: int = 0, limit: int = 100):
+        return db.query(self.model).offset(skip).limit(limit).all()
--- App/controllers/user_controller.py
+++ App/controllers/user_controller.py
@@ -1,34 +1,53 @@
 from sqlalchemy.orm import Session
+from app.controllers.base_controller import BaseController
 from app.models.user import User
 from app.utils.enums import UserRole
 from app.utils.security import verify_password
 from fastapi import HTTPException, status
 
-def get_by_role(db: Session, role: UserRole):
-    return db.query(User).filter(User.role == role.role).all()
 
-def update_role(db: Session, user_id: int, new_role: UserRole):
-    user = db.query(User).filter(User.id == user_id).first()
+class UserController(BaseController):
 
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+    def create_user(db: Session, user: User):
+        return super().create(db, user)
+    
+    def update_user(db: Session, user_id: int, user: User):
+        return super().update(db, user_id, user)
+    
+    def delete_user(db: Session, user_id: int):
+        return super().delete(db, user_id)
+    
+    def get_multi(db: Session, skip: int = 0, limit: int = 100):
+        return super().get_multi(db, skip, limit)
+    
+    def get_by_id(db: Session, user_id: int):
+        return super().get_by_id(db, user_id)
+    
+    def get_by_role(db: Session, role: UserRole):
+        return db.query(User).filter(User.role == role).all()
+    
+    def update_role(db: Session, user_id: int, new_role: UserRole):
+        user = db.query(User).filter(User.id == user_id).first()
 
-    user.role = new_role.role
-    db.add(user)
-    db.commit()
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
 
-    return user 
+        user.role = new_role.role
+        db.add(user)
+        db.commit()
 
-def get_by_email(db: Session, email: str):
-    user = db.query(User).filter(User.email == email).first()
+        return user 
 
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
-    return user
+    def get_by_email(db: Session, email: str):
+        user = db.query(User).filter(User.email == email).first()
 
-def authenticate(db: Session, email:str, pasword: str):
-    user = get_by_email(db, email)
+        if not user:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User not found')
+        return user
 
-    if not user or not verify_password(pasword, user.password):
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
-    return user
+    def authenticate(db: Session, email:str, pasword: str):
+        user = self.get_by_email(db, email)
+
+        if not user or not verify_password(pasword, user.password):
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid credentials')
+        return user
